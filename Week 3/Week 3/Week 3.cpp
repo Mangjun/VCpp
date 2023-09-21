33d1f69e5950872cac205d321cafa0ebde2f0d82
Mangjun <myoungjun55@naver.com>
2023-09-21 15:35:54 +0900

    VCpp 3주차 과제 수정

--- Week 3/Week 3/Week 3.cpp
+++ Week 3/Week 3/Week 3.cpp
@@ -1,6 +1,7 @@
 ﻿#include <stdio.h>
+#include <iostream>
 #include <conio.h>
-#include <Windows.h>
+#include <chrono>
 
 #define UP 72 // -32 + H 998
 #define DOWN 80 // -32 + P 999
@@ -15,11 +16,36 @@ void clear(int array[], int n)
 	}
 }
 
+void timer(std::chrono::time_point<std::chrono::system_clock>& time1, std::chrono::time_point<std::chrono::system_clock>& time2, int result[], int& index, char& ch)
+{
+	time2 = std::chrono::system_clock::now(); // Timer()가 호출된 시간
+	std::chrono::duration<double, std::milli> elapsed_time = time2 - time1; // 입력된 시간 구하기
+
+	if (elapsed_time.count() >= 300.0) {
+		printf("\nTimeout!\n");
+		// 문자 배열 초기화
+		if (result[index] == -32) {
+			clear(result, index);
+			result[0] = -32;
+			index = 1;
+			ch = ' '; // 문자 초기화
+		}
+		else {
+			clear(result, index);
+			index = 0;
+			ch = ' '; // 문자 초기화
+		}
+
+	}
+	time1 = time2; // 시간 재설정
+}
+
 int main()
 {
 	char ch = ' '; // 사용자에게 입력 받음
-	int result[100] = {}; // 입력받은 문자를 저장할 배열
+	int result[100] = { }; // 입력받은 문자를 저장할 배열
 	int index = 0; // 배열의 입력된 문자 수
+	std::chrono::time_point<std::chrono::system_clock> time1, time2;
 
 	while (ch != 27) {
 		// 문자 배열이 꽉 찬 경우 프로그램 종료
@@ -27,12 +53,14 @@ int main()
 			break;
 		} else if (index >= 5) {
 				for(int i=0; i < index; i++) {
+					timer(time1, time2, result, index, ch);
 
 					// 좌 상 우 하
 					if ((result[i] == -32 && result[i + 1] == 996) && (result[i+2] == -32 && result[i + 3] == 998) && (result[i+4] == -32 && result[i + 5] == 997) && (result[i+6] == -32 && result[i + 7] == 999)) {
 						printf("\n(* v *)\n");
 						clear(result, index);
 						index = 0;
+						ch = ' ';
 					}
 
 					// 하 우 a
@@ -40,19 +68,25 @@ int main()
 						printf("\n아도겐! =o\n");
 						clear(result, index);
 						index = 0;
+						ch = ' ';
 					}
 
 					// 우 하 우 f
 					if ((result[i] == -32 && result[i+1] == 997) && (result[i+2] == -32 && result[i + 3] == 999) && (result[i+4] == -32 && result[i + 5] == 997) && (char)result[i + 6] == 'f') {
 						printf("\n뻐큐! ( -_-)-ㅗ\n");
 						clear(result, index);
-						index = 0; 
+						index = 0;
+						ch = ' ';
 					}
 
 				}
 		}
 
 		ch = _getch();
+
+		if (ch != ' ') {
+			auto time1 = std::chrono::system_clock::now();
+		}
 		
 		// 숫자를 준 이유는 알파벳과 구분하기 위함
 		switch (ch)
@@ -113,7 +147,6 @@ int main()
 			break;
 		}
 
-		Sleep(300);
 	}
 
 }

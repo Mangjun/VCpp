eba8930223115e4f9bdfd7d1356028115f138b3a
Mangjun <myoungjun55@naver.com>
2023-09-22 10:30:33 +0900

    VCpp 3주차 과제 다시 변경

--- Week3/Week3/Week3.cpp
+++ Week3/Week3/Week3.cpp
@@ -1,7 +1,6 @@
 ﻿#include <stdio.h>
-#include <iostream>
 #include <conio.h>
-#include <chrono>
+#include <Windows.h>
 
 #define UP 72 // -32 + H 998
 #define DOWN 80 // -32 + P 999
@@ -9,46 +8,18 @@
 #define RIGHT 77 // -32 + M 997
 
 // 문자 배열을 비움
-void clear(int array[], int& index, char& ch)
+void clear(int array[], int n)
 {
-	for (int i = 0; i < index; i++) {
+	for (int i = 0; i < n; i++) {
 		array[i] = 0;
 	}
-
-	index = 0;
-	ch = ' ';
-}
-
-/**
-* 고쳐야 할 점: 방향키가 입력됐을 때 -32와 뒤 숫자가 들어오는데 -32에서 짤려서 문자가 출력됨
-*/
-void timer(std::chrono::time_point<std::chrono::system_clock>& time1, std::chrono::time_point<std::chrono::system_clock>& time2, int result[], int& index, char& ch)
-{
-	time2 = std::chrono::system_clock::now(); // Timer()가 호출된 시간
-	std::chrono::duration<double, std::milli> elapsed_time = time2 - time1; // 입력된 시간 구하기
-
-	if (elapsed_time.count() >= 300.0) {
-		printf("\nTimeout!\n");
-		if (result[index] == -32) {
-			clear(result, index, ch);
-			result[0] = -32;
-			index = 1;
-		}
-		else {
-			clear(result, index, ch);
-			index = 0;
-		}
-
-	}
-	time1 = time2; // 시간 재설정
 }
 
 int main()
 {
 	char ch = ' '; // 사용자에게 입력 받음
-	int result[100] = { }; // 입력받은 문자를 저장할 배열
+	int result[100] = {}; // 입력받은 문자를 저장할 배열
 	int index = 0; // 배열의 입력된 문자 수
-	std::chrono::time_point<std::chrono::system_clock> time1, time2; // time1은 입력됐을 때 시간, time2는 입력이 완료된 시점에서 시간
 
 	while (ch != 27) {
 		// 문자 배열이 꽉 찬 경우 프로그램 종료
@@ -57,34 +28,32 @@ int main()
 		}
 		else if (index >= 5) {
 			for (int i = 0; i < index; i++) {
-				timer(time1, time2, result, index, ch);
 
 				// 좌 상 우 하
 				if ((result[i] == -32 && result[i + 1] == 996) && (result[i + 2] == -32 && result[i + 3] == 998) && (result[i + 4] == -32 && result[i + 5] == 997) && (result[i + 6] == -32 && result[i + 7] == 999)) {
 					printf("\n(* v *)\n");
-					clear(result, index, ch);
+					clear(result, index);
+					index = 0;
 				}
 
 				// 하 우 a
 				if ((result[i] == -32 && result[i + 1] == 999) && (result[i + 2] == -32 && result[i + 3] == 997) && (char)result[i + 4] == 'a') {
 					printf("\n아도겐! =o\n");
-					clear(result, index, ch);
+					clear(result, index);
+					index = 0;
 				}
 
 				// 우 하 우 f
 				if ((result[i] == -32 && result[i + 1] == 997) && (result[i + 2] == -32 && result[i + 3] == 999) && (result[i + 4] == -32 && result[i + 5] == 997) && (char)result[i + 6] == 'f') {
 					printf("\n뻐큐! ( -_-)-ㅗ\n");
-					clear(result, index, ch);
+					clear(result, index);
+					index = 0;
 				}
 
 			}
 		}
 
 		ch = _getch();
-
-		if (ch != ' ') {
-			auto time1 = std::chrono::system_clock::now();
-		}
 
 		// 숫자를 준 이유는 알파벳과 구분하기 위함
 		switch (ch)
@@ -145,6 +114,7 @@ int main()
 			break;
 		}
 
+		Sleep(300);
 	}
 
 }
